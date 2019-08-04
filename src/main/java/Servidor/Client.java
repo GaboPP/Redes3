4ef9f0fc72a0d9adaf117d91d5c39896876dac23
gastorga <gabriel.astorga@sansano.usm.cl>
2019-08-04 13:44:18 -0400

    yian + us

--- src/main/java/Servidor/Client.java
+++ src/main/java/Servidor/Client.java
@@ -5,6 +5,54 @@ import org.json.*;
 import java.util.concurrent.TimeUnit;
 
 public class Client {
+    public static void descargar(String filename, String host){
+        String path = System.getProperty("user.dir");
+        //File file = new File(path + "/" + filename);
+        int fileSize = 64*1024*1024; //16 mega bytes
+        int bytesRead;
+        int current = 0;
+        int puerto = 5900;
+        Socket socketCliente = null;
+
+        try {
+            socketCliente = new Socket(host, puerto);
+            System.out.println (host);
+            BufferedReader br = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+            String confirmacion = br.readLine();
+
+            if((confirmacion.equals("ERROR"))){
+                System.out.println("El archivo no esta disponible en estos momentos, intentelo mas tarde");
+                socketCliente.close();
+                return;
+            }
+
+            System.out.println(confirmacion);
+            byte []byteArray = new byte [fileSize];
+            InputStream inputStream = socketCliente.getInputStream();
+            FileOutputStream fileOutput = new FileOutputStream(path + "/" + filename);
+            BufferedOutputStream bufferedOutput = new BufferedOutputStream(fileOutput);
+            bytesRead = inputStream.read(byteArray, 0, byteArray.length);
+            current = bytesRead;
+
+            do{
+                bytesRead = inputStream.read(byteArray, current, (byteArray.length - current));
+                if (bytesRead >= 0) current += bytesRead;
+            }while(bytesRead > -1);
+
+            bufferedOutput.write(byteArray, 0, current);
+            bufferedOutput.flush();
+            System.out.println("DESCARGA COMPLETA");
+
+            inputStream.close();
+            fileOutput.close();
+            bufferedOutput.close();
+            socketCliente.close();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
     public void run() throws IOException {
 
 
@@ -26,24 +74,31 @@ public class Client {
             String some;
             try {
                 fromServer = new JSONObject(in.readLine());
-                while (!fromServer.getString("message").equals("Bye.")) {
+                message = fromServer.getString("message");
+                while (!message.equals("Bye.")) {
+                    message = fromServer.getString("message");
                     if (fromServer.opt("response") != null) {
                         response = fromServer.getJSONArray("response");
                         System.out.println("Server: " + response);
                     }
                     if (fromServer.opt("message").equals("Done!")){
-                        out.println("Arigato!");
+                        out.println("ack");
                         message = fromServer.getString("message");
                         System.out.println("Server: " + message);
                         fromServer = new JSONObject(in.readLine());
                     }
-                    message = fromServer.getString("message");
                     System.out.println("Server: " + message);
                     // System.out.println("empezamos denuevo");
 
                     fromUser = stdIn.readLine();
                     boolean flag = true;
-
+                    if (fromUser.equals("2")){
+                        fromUser = stdIn.readLine();
+                        out.println("2 " + fromUser);
+                        descargar(fromUser, hostName);
+                        message = fromServer.getString("message");
+                        System.out.println("Server: " + message);
+                    }
                     if (fromUser != null) {
                         out.println(fromUser);
                         TimeUnit.SECONDS.sleep(1);
--- src/main/java/Servidor/protocol.java
+++ src/main/java/Servidor/protocol.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Hashtable;
+import java.net.Socket;
+import java.util.List;
 
 import org.bytedeco.javacv.*;
 import org.bytedeco.javacv.Frame;
@@ -29,12 +31,140 @@ public class protocol {
     private String disponibilidad = "";
     int portNumber = 6666;
     String host =  "localhost"; //CAMBIAR SOLO USADO PARA TESTIN'!!!!!!!!!!!!!!
+    public void descargar(String filename, Socket socket){
+        String path = System.getProperty("user.dir");
+        Socket dataSocket = null;
+        BufferedReader dataIn = null;
 
+        List<Socket> listDataSockets = new ArrayList<Socket>();
+        List<BufferedReader> dataBuffers = new ArrayList<BufferedReader>();
+
+        byte[] byteArray = new byte[64*1024*1024];
+        String linea = null;
+        String confirmacion = null;
+        Base64.Decoder decoder = Base64.getDecoder();
+
+        List<String> stringsBase64 = new ArrayList<String>();
+
+        try{
+            System.out.println ("FGSFDS");
+            respuesta(socket, "COMIENZA LA DESCARGA");
+            ServerSocket serverSocket = new ServerSocket(5900);
+            Socket socketCliente = serverSocket.accept();
+
+            String line = null;
+            String[] parts = null;
+            int totalSize = 0;
+            String response = null;
+            String[] address = null;
+//            while ((line = br.readLine()) != null) {
+//                parts = line.split(" ");
+//                if (parts[0].equals(filename)){
+//                    totalSize = Integer.parseInt(parts[1]);
+//                } else {
+//                    response = "El archivo no existe";
+//                }
+//            }
+//            br.close();
+
+            ByteArrayOutputStream byteWriter = new ByteArrayOutputStream();
+            int index = 0;
+            int counter = 0;
+
+            respuesta(socketCliente, "DONE");
+
+            for(int i = 0; i < listDataSockets.size(); ++i){
+                if(listDataSockets.get(i).isConnected())
+                    respuesta(listDataSockets.get(i), "DONE");
+            }
+
+            byteArray = byteWriter.toByteArray();
+
+            OutputStream output = socketCliente.getOutputStream();
+            output.write(byteArray, 0, totalSize);
+            output.flush();
+
+            for(int i = 0; i < listDataSockets.size(); ++i){
+                if(listDataSockets.get(i).isConnected()){
+                    (listDataSockets.get(i)).close();
+                    (dataBuffers.get(i)).close();
+                }
+            }
+
+            output.close();
+            serverSocket.close();
+            socketCliente.close();
+            System.out.println("Done");
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void respuesta(Socket socket, String mensaje){
+        PrintStream response = null;
+
+        try{
+            response = new PrintStream(socket.getOutputStream());
+            response.println(mensaje);
+            response.flush();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void  show_video(String Name) throws IOException {
+        //Canvas para mostrar el video
+        CanvasFrame canvas = new CanvasFrame("VideoCanvas");
+        //setearlo para cerrar canvas
+        canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+
+        FrameGrabber grabber = new OpenCVFrameGrabber ("src/main/java/Servidor/media/" + Name);//video4.mp4
+
+        // Abriendo el vidio
+        grabber.start ();
+        IplImage imagen;
+        Frame frame;
+        while ((frame = grabber.grab()) != null) {
+
+            // frame se pueden convertir a bufferedimage
+            Java2DFrameConverter bimConverter = new Java2DFrameConverter();
+            BufferedImage img = bimConverter.convert(frame);
+            BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+            Graphics2D bGr = bimage.createGraphics();
+            bGr.drawImage(img, 0, 0, null);
+            bGr.dispose();
+
+            img.flush();
+            //la buffered image a bytearrayoutputstream
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            // eso se mete en un imageIO
+            ImageIO.write(bimage, "png", outputStream);
+            //imageIO se puede pasar a bytearray
+            String encodedString = Base64.getEncoder().encodeToString(outputStream.toByteArray());
+            System.out.println (encodedString.length ());
+
+            // MANDAR DATAGRAMAS
+            // output.writeUTF(encodedString);
+
+            byte[] decode = Base64.getDecoder( ).decode(encodedString);
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(decode);
+            BufferedImage Bimage = ImageIO.read (inputStream);
+            Frame frame2 = bimConverter.convert(Bimage);
+
+            // Set canvas size as per dimentions of video frame.
+            // canvas.setCanvasSize(grabber.getImageWidth(), grabber.getImageHeight());
+            if (frame2 != null) {
+                //Show video frame in canvas
+                canvas.showImage(frame2);
+            }        }
+        // Close the video file
+        grabber.release();
+    }
     private String options_message = "\nEscoja una acción:\n" + "1- Mostrar videos disponibles\n" + "2- Reproducir Video\n" + "3- Salir de la App\n"+
             "4- Detener reproduccion actual";
 
     public Object processInput(String theInput, Socket socket) throws IOException {
-
+        System.out.println (theInput);
         JSONObject theOutput = new JSONObject();
         ArrayList<String> directorio;
         if (state == validate_p) {
@@ -51,23 +181,19 @@ public class protocol {
                 try {// Mostrar videos disponibles
 
                     System.out.println("Videos Disponibles:");
+                    System.out.println("working on =" + System.getProperty("user.dir"));
 
-                    // System.out.println("working on =" + System.getProperty("user.dir"));
-
-                    File folder = new File("./media");
+                    File folder = new File("./src/main/java/Servidor/media");
                     File[] listOfFiles = folder.listFiles();
                     ArrayList<String> listOfFilesNames = new ArrayList<>();
 
                     for (int i = 0; i < listOfFiles.length; i++) {
-                        System.out.println(listOfFiles[i].getName());
                         listOfFilesNames.add(listOfFiles[i].getName());
-
                     }
 
                     theOutput.put("message", disponibilidad + options_message);
                     System.out.println(listOfFilesNames);
                     theOutput.put("response", listOfFilesNames);
-                    // System.out.println(theOutput);
 
 
                 } catch (JSONException e) {
@@ -76,64 +202,9 @@ public class protocol {
                 CurrentCommand = 0;
                 state = sentcommands;
             }
-            else if (theInput.equals("2")) { // Reproducir Video
-                //Canvas para mostrar el video
-                CanvasFrame canvas = new CanvasFrame("VideoCanvas");
-                //setearlo para cerrar canvas
-                canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-
-                FrameGrabber grabber = new OpenCVFrameGrabber ("src/main/java/Servidor/media/video4.mp4");
-
-                // Abriendo el vidio
-                grabber.start ();
-                IplImage imagen;
-                Frame frame;
-                while ((frame = grabber.grab()) != null) {
-
-                    // frame se pueden convertir a bufferedimage
-                    Java2DFrameConverter bimConverter = new Java2DFrameConverter();
-                    BufferedImage img = bimConverter.convert(frame);
-                    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-                    Graphics2D bGr = bimage.createGraphics();
-                    bGr.drawImage(img, 0, 0, null);
-                    bGr.dispose();
-
-                    img.flush();
-                    //la buffered image a bytearrayoutputstream
-                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                    // eso se mete en un imageIO
-                    ImageIO.write(bimage, "png", outputStream);
-                    //imageIO se puede pasar a bytearray
-                    String encodedString = Base64.getEncoder().encodeToString(outputStream.toByteArray());
-                    System.out.println (encodedString.length ());
-
-                    // MANDAR DATAGRAMAS
-                    // output.writeUTF(encodedString);
-
-                    byte[] decode = Base64.getDecoder( ).decode(encodedString);
-                    ByteArrayInputStream inputStream = new ByteArrayInputStream(decode);
-                    BufferedImage Bimage = ImageIO.read (inputStream);
-                    Frame frame2 = bimConverter.convert(Bimage);
-
-                    // Set canvas size as per dimentions of video frame.
-                    // canvas.setCanvasSize(grabber.getImageWidth(), grabber.getImageHeight());
-                    if (frame2 != null) {
-                        //Show video frame in canvas
-                        canvas.showImage(frame2);
-                    }
-
-
-                }
-                // Close the video file
-                grabber.release();
-                //BufferedReader scanner_play_video = new BufferedReader(new InputStreamReader(System.in));
-                System.out.println("Escriba nombre de video a reproducir:");
-                //String video2play = scanner_play_video.readLine();
-                // Abrir archivo y todo el tema para transmitirlo
-
-
-                //ou.close();
-                //DSocket.close();
+            else if (theInput.split (" ")[0].equals("2")) { // Reproducir Video
+                String Name = theInput.split (" ")[1];
+                descargar (Name, socket);
             }
 
             else if (theInput.equals("3")) { // Salir de la App
